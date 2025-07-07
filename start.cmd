59aa3cdbdc90e7209c960a76a5808f4df1f95083
kabzon93region <kabzon93region@users.noreply.github.com>
2025-07-08 00:29:07 +0300

    start.cmd

---
+++ start.cmd
@@ -0,0 +1,20 @@
+@echo off
+@chcp 65001
+setlocal enabledelayedexpansion
+set "PYTHONUTF8=1"
+
+REM --- каталог скрипта ---
+pushd "%~dp0"
+
+REM --- виртуальное окружение ---
+set "VENV_DIR=%~dp0vo_venv"
+if not exist "%VENV_DIR%\Scripts\python.exe" (
+    python -m venv "%VENV_DIR%"
+)
+call "%VENV_DIR%\Scripts\activate"
+
+echo Готово. Запускаем программу.
+python vo.py
+
+echo Программа завершилась.
+pause
